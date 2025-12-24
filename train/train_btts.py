588006fc9eb957002a12a11aebb8e79ae7b5079a
jgpradol-ops <jg.pradol@gmail.com>
2025-12-23 19:18:05 -0600

    Eliminar train_btts.py

--- train/train_btts.py
+++
@@ -1,8 +0,0 @@
-import joblib, os
-from sklearn.linear_model import LogisticRegression
-
-X = [[2,1],[0,0],[3,2],[1,1]]
-y = [1,0,1,1]
-
-os.makedirs("models", exist_ok=True)
-joblib.dump(LogisticRegression().fit(X, y), "models/btts.pkl")
